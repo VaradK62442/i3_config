14e535f9630e660f6607bec6f11710b490bcc9a2
VaradK62442 <varadkulk123@gmail.com>
2025-04-23 18:58:07 +0100

    use connected flags to properly manage line reading

--- bt_media_watcher.sh
+++ bt_media_watcher.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-dbus-monitor --system "type='signal',interface='org.freedesktop.DBus.Properties" |
+connected_flag=0
+echo "started" > ~/.bt_log
+
+dbus-monitor --system "type='signal',interface='org.freedesktop.DBus.Properties'" |
 while read -r line; do
-    if echo "$line" | grep -q "$buds" && echo "$line" | grep -q "Connected" && echo "$line" | grep -q "boolean false"; then
+    if echo "$line" | grep -q "Connected"; then
+        echo "connected" >> ~/.bt_log
+        connected_flag=1
+    elif [[ $connected_flag -eq 1 ]] && echo "$line" | grep -q "boolean false"; then
+        echo "bool false" >> ~/.bt_log
         playerctl pause
+        connected_flag=0
+    elif [[ $connected_flag -eq 1 ]] && echo "$line" | grep -q "boolean true"; then
+        echo "bool true" >> ~/.bt_log
+        playerctl play
+        connected_flag=0
     fi
 done
